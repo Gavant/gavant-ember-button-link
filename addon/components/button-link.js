586f6b022778cba602fb7bd65c28eb99a9e98ea7
Gabriella Spain <gspain@gavant.com>
2019-05-28 14:00:28 -0400

    Updated export urls

--- addon/components/button-link.js
+++ addon/components/button-link.js
@@ -1,7 +1,7 @@
 import LinkComponent from '@ember/routing/link-component';
 import { alias } from '@ember/object/computed';
-import Button from 'gavant-ember-button-basic/mixins/button';
-import layout from 'gavant-ember-button-basic/templates/components/button-basic';
+import Button from '@gavant/ember-button-basic/mixins/button';
+import layout from '@gavant/ember-button-basic/templates/components/button-basic';
 
 export default LinkComponent.extend(Button, {
   layout,
